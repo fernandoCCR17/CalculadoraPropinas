9e618e72fedddb48c228435264a8fb4cef5e9d12
Fernando De la Cruz Ramos <fernandodelacruzramos2001@gmail.com>
2024-07-10 22:17:51 -0600

    Iniciando proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { menuItems } from "./data/db"
 
 function App() {
+  console.log(menuItems);
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
